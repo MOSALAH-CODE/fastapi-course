26584346a91065c9259eec35a1e2042e42f0c129
Mohammad Salah <m.salah@alifbee.com>
2024-02-29 22:05:28 +0300

    Implemented Caching System for User logs

--- app/blog/routers/user.py
+++ app/blog/routers/user.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter
 from .. import database, schemas, models, oauth2
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, status
+from fastapi.responses import JSONResponse
 from ..repository import user
 from typing import List
+from app import main
+import json
 
 router = APIRouter(
     prefix="/user",
@@ -36,4 +38,19 @@ def update_user(request: schemas.UserUpdate, db: Session = Depends(get_db), curr
 
 @router.get('/logs', response_model=List[schemas.UserLog])
 def get_user_logs(db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
-    return user.get_logs(current_user, db)
+    cache = main.rd.get("user-log")
+    if cache:
+        cached_logs = json.loads(cache.decode('utf-8'))
+        return [schemas.UserLog(**log) for log in cached_logs]
+    else:
+        user_logs = user.get_logs(current_user, db)
+        serialized_logs = []
+        for log in user_logs:
+            serialized_logs.append({
+                "user_id": log.user.id,
+                "event_type": log.event_type,
+                "description": log.description
+            })
+        serialized_logs_bytes = json.dumps(serialized_logs).encode('utf-8')
+        main.rd.setex('user-log', 30, serialized_logs_bytes)
+        return user_logs
--- app/main.py
+++ app/main.py
@@ -3,6 +3,12 @@ from .blog import models
 from .blog.database import engine
 from .blog.routers import blog, user, authentication
 
+import redis
+import json
+
+rd = redis.Redis(host= "localhost", port=6379, db=0)
+
+
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
@@ -10,3 +16,17 @@ models.Base.metadata.create_all(engine)
 app.include_router(authentication.router)
 app.include_router(blog.router)
 app.include_router(user.router)
+
+
+# @app.get('/test')
+# def  test():
+#     cache = rd.get('test1')
+#     print(cache)
+#     if cache:
+#         print("cache hit")
+#         return {'msg': json.loads(cache)}
+#     else:
+#         rd.set("test1","123")
+#         rd.expire("test1", 5)
+#         print("cache mis")
+#         return {"msg": 'Hello World'}
